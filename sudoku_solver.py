bd494fcc3b4ee37a200782d5966ed7809a0ff4b4
Isabel Lee <ijl0322@gmail.com>
2016-04-05 16:21:52 +0800

    buggy main
    still working on backtracking

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -14,27 +14,28 @@ def print_board(board):
             print "+-------+-------+-------+"
             
         
-def check_col(board, x, y):
-    num = board[y][x]
+def check_col(board, x, y, num):
+    #print "\nchecking col......"
     for i in range(9):
         if i != y:
+            #print board[i][x],
             if board[i][x] == num:
                 return False
     return True
       
-def check_row(board, x, y):
-    num = board[y][x]
+def check_row(board, x, y, num):
+    #print "\nchecking row......"
     for i in range(9):
         if i != x:
-            print board[y][i],
+            #print board[y][i],
             if board[y][i] == num:
                 return False
     return True     
     
-def check_box(board, x, y):
+def check_box(board, x, y, num):
+    #print "\nchecking box......"
     num_list = [[0,1,2],[3,4,5],[6,7,8]]
-    num = board[y][x]
-    print num
+
     for i in num_list:
         if x in i:
             row = i
@@ -43,12 +44,24 @@ def check_box(board, x, y):
     for i in row:
         for j in col:
             if not (i == x and j == y):
+                #print board[j][i],
                 if num == board[j][i]:
                     return False
     return True
             
-    
-    
+def check_all(board, x, y, num):
+    if check_row(board, x, y, num) and check_col(board, x, y, num) and check_box(board, x, y, num):
+        return True
+    return False                    
+                            
+def fill(board, x, y, num):
+    if board[y][x] != 0:
+        return board
+    for i in range(1,10):
+        if i != num and check_all(board, x, y, i):
+            board[y][x] = i
+            return board
+
 
 board = [[0,4,0,6,3,9,0,5,0],\
          [0,0,0,0,0,0,0,0,0],\
@@ -60,16 +73,38 @@ board = [[0,4,0,6,3,9,0,5,0],\
          [0,0,4,0,0,0,9,0,0],\
          [9,0,0,2,0,3,0,0,5]]
          
-test_case = [[1,3,5,7,2,4,6,8,9],\
-             [2,0,0,0,0,0,0,0,0],\
-             [3,0,0,0,0,0,0,0,0],\
-             [4,0,0,0,0,0,0,0,0],\
-             [5,0,0,0,0,0,0,0,0],\
-             [6,0,0,0,0,0,0,0,0],\
-             [7,0,0,0,0,0,1,2,3],\
-             [8,0,0,0,0,0,4,5,6],\
-             [9,0,0,0,0,0,7,8,9]]   
-                   
-#print check_row(test_case, 0, 0) 
-print check_box(test_case, 6, 8)        
-print_board(test_case)
+
+solution = board[:]                   
+#print_board(board)
+
+def main():
+    solution = board[:]  
+    x = 0
+    y = 0
+
+    while x < 8 or y < 8 :
+        print x, y
+        result = fill(solution, x, y, solution[y][x])
+        if result == None:
+            if x <= 8 and x > 0:
+                x -= 1
+            elif x == 0 and y > 0:
+                x = 8
+                y -= 1  
+    
+        #go forward
+
+        else:
+            solution = result     
+            if x < 8:
+                x += 1
+            elif x == 8 and y < 8:
+                x = 0
+                y += 1
+
+        print_board(solution)         
+
+    
+    
+main()
+
