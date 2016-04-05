a6208715a1cafce1f758dac7c6c5ddb46c30640f
Isabel Lee <ijl0322@gmail.com>
2016-04-05 20:02:24 +0800

    working solution - array

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -13,6 +13,12 @@ def print_board(board):
         if i in [2,5,8]:
             print "+-------+-------+-------+"
             
+def is_full(board):
+    for x in range(0, 9):
+        for y in range (0, 9):
+            if board[y][x] == 0:
+                return False
+    return True            
         
 def check_col(board, x, y, num):
     #print "\nchecking col......"
@@ -54,13 +60,60 @@ def check_all(board, x, y, num):
         return True
     return False                    
                             
-def fill(board, x, y, num):
-    if board[y][x] != 0:
-        return board
+def fill(board, x, y):
+    possible = {}
     for i in range(1,10):
-        if i != num and check_all(board, x, y, i):
-            board[y][x] = i
-            return board
+        possible[i] = 0
+        if check_all(board, x, y, i):
+            possible[i] = i
+    if board[y][x] != 0:
+        possible[(board[y][x])] = 0
+    return possible
+        
+def possibleEntries(board, i, j):
+    
+    possibilityArray = {}
+    
+    for x in range (1, 10):
+        possibilityArray[x] = 0
+    
+    #For horizontal entries
+    for y in range (0, 9):
+        if not board[i][y] == 0: 
+            possibilityArray[board[i][y]] = 1
+     
+    #For vertical entries
+    for x in range (0, 9):
+        if not board[x][j] == 0: 
+            possibilityArray[board[x][j]] = 1
+            
+    #For squares of three x three
+    k = 0
+    l = 0
+    if i >= 0 and i <= 2:
+        k = 0
+    elif i >= 3 and i <= 5:
+        k = 3
+    else:
+        k = 6
+    if j >= 0 and j <= 2:
+        l = 0
+    elif j >= 3 and j <= 5:
+        l = 3
+    else:
+        l = 6
+    for x in range (k, k + 3):
+        for y in range (l, l + 3):
+            if not board[x][y] == 0:
+                possibilityArray[board[x][y]] = 1          
+    
+    for x in range (1, 10):
+        if possibilityArray[x] == 0:
+            possibilityArray[x] = x
+        else:
+            possibilityArray[x] = 0
+    
+    return possibilityArray
 
 
 board = [[0,4,0,6,3,9,0,5,0],\
@@ -74,37 +127,48 @@ board = [[0,4,0,6,3,9,0,5,0],\
          [9,0,0,2,0,3,0,0,5]]
          
 
-solution = board[:]                   
+        
 #print_board(board)
 
-def main():
-    solution = board[:]  
-    x = 0
-    y = 0
-
-    while x < 8 or y < 8 :
-        print x, y
-        result = fill(solution, x, y, solution[y][x])
-        if result == None:
-            if x <= 8 and x > 0:
-                x -= 1
-            elif x == 0 and y > 0:
-                x = 8
-                y -= 1  
+def solver(board):
+    i = 0
+    j = 0
     
-        #go forward
-
-        else:
-            solution = result     
-            if x < 8:
-                x += 1
-            elif x == 8 and y < 8:
-                x = 0
-                y += 1
-
-        print_board(solution)         
-
+    possiblities = {}
     
-    
-main()
+    # if board is full, there is no need to solve it any further
+    if is_full(board):
+        print("Board Solved Successfully!")
+        print_board(board)
+        return
+    else:
+        # find the first vacant spot
+        for x in range (0, 9):
+            for y in range (0, 9):
+                if board[x][y] == 0:
+                    i = x
+                    j = y
+                    break
+            else:
+                continue
+            break
+        
+        # get all the possibilities for i,j
+        possiblities = fill(board, j, i)
 
+        
+        # go through all the possibilities and call the the function
+        # again and again
+        for x in range (1, 10):
+            if possiblities[x] != 0:
+                board[i][j] = possiblities[x]
+                #file.write(printFileBoard(board))
+                solver(board)
+        # backtrack
+        board[i][j] = 0
+    
+        
+    
+solver(board)
+
+        
