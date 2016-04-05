dda7dbec5da36315c0b8f248d4e02d7e340df13c
Isabel Lee <ijl0322@gmail.com>
2016-04-05 14:27:18 +0800

    check functions implemented

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -14,7 +14,39 @@ def print_board(board):
             print "+-------+-------+-------+"
             
         
-        
+def check_col(board, x, y):
+    num = board[y][x]
+    for i in range(9):
+        if i != y:
+            if board[i][x] == num:
+                return False
+    return True
+      
+def check_row(board, x, y):
+    num = board[y][x]
+    for i in range(9):
+        if i != x:
+            print board[y][i],
+            if board[y][i] == num:
+                return False
+    return True     
+    
+def check_box(board, x, y):
+    num_list = [[0,1,2],[3,4,5],[6,7,8]]
+    num = board[y][x]
+    print num
+    for i in num_list:
+        if x in i:
+            row = i
+        if y in i:
+            col = i
+    for i in row:
+        for j in col:
+            if not (i == x and j == y):
+                if num == board[j][i]:
+                    return False
+    return True
+            
     
     
 
@@ -28,4 +60,16 @@ board = [[0,4,0,6,3,9,0,5,0],\
          [0,0,4,0,0,0,9,0,0],\
          [9,0,0,2,0,3,0,0,5]]
          
-print_board(board)
+test_case = [[1,3,5,7,2,4,6,8,9],\
+             [2,0,0,0,0,0,0,0,0],\
+             [3,0,0,0,0,0,0,0,0],\
+             [4,0,0,0,0,0,0,0,0],\
+             [5,0,0,0,0,0,0,0,0],\
+             [6,0,0,0,0,0,0,0,0],\
+             [7,0,0,0,0,0,1,2,3],\
+             [8,0,0,0,0,0,4,5,6],\
+             [9,0,0,0,0,0,7,8,9]]   
+                   
+#print check_row(test_case, 0, 0) 
+print check_box(test_case, 6, 8)        
+print_board(test_case)
